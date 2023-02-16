a436260ee1ec1f918c5a394c0eef5a1f4abb787c
NikaEliashvili <nikaeliashvili18@outlook.com>
2023-02-16 16:41:52 +0400

    Finished version(1.1)

--- index.js
+++ index.js
@@ -2,69 +2,96 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-const savedBtn = document.getElementById("saved-btn");
-let showed = document.getElementById("showed");
 let saved = document.getElementById("saved");
 const clearBtn = document.getElementById("clear-btn");
-let cleared = document.getElementById("cleared");
+const cleared = document.getElementById("cleared");
+const savetabBtn = document.getElementById("savetab-btn");
+const savedtab = document.getElementById("savedtab");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 cleared.style.visibility = "hidden";
 saved.style.visibility = "hidden";
-showed.style.visibility = "hidden";
+savedtab.style.visibility = "hidden";
+clearBtn.style.visibility = "visible";
+savetabBtn.style.visibility = "visible";
+inputBtn.style.visibility = "visible";
 
-//Showing All Links Before Clearing
-savedBtn.addEventListener("click", function () {
-  renderLeads();
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+// SAVING ACTIVE TAB LINK
+savetabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
+
+// rendering of leads
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li><a target="_blank" href=${leads[i]} >${leads[i]} </a></li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
 
 //clearing
 clearBtn.addEventListener("click", function () {
-  for (i = myLeads.length; i >= 0; i--) {
-    myLeads.shift();
-  }
+  localStorage.clear();
+  myLeads = [];
   ulEl.textContent = "";
 });
 
 // saving and printing
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML = `<li><a target="_blank" href=${myLeads[i]} >${myLeads[i]} </a></li>`;
+  if (inputEl.value) {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
   }
-  //renderLeads();
 });
 
-// rendering of leads
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    // Refactor the code below to use a template string
-    listItems += `<li><a target="_blank" href=${myLeads[i]} >${myLeads[i]} </a></li>`;
-  }
-  ulEl.innerHTML = listItems;
-}
-
-//"showed" links NOTIFICATION
-savedBtn.addEventListener("click", function () {
-  showed.style.visibility = "visible";
-  setTimeout(function () {
-    showed.style.visibility = "hidden";
-  }, 390);
-});
+savetabBtn.addEventListener("click", function () {});
 
 // "cleared" NOTIFICATION
 clearBtn.addEventListener("click", function () {
-  cleared.style.visibility = "visible";
+  clearBtn.style.visibility = "hidden";
+  setTimeout(function () {
+    cleared.style.visibility = "visible";
+  }, 350);
+
   setTimeout(function () {
     cleared.style.visibility = "hidden";
-  }, 390);
+    clearBtn.style.visibility = "visible";
+  }, 990);
 });
 
-// "saved" NOTIFICATION
+// "saved" NOTIFICATION ( inputBtn and saved)
 inputBtn.addEventListener("click", function () {
-  saved.style.visibility = "visible";
+  inputBtn.style.visibility = "hidden";
+  setTimeout(function () {
+    saved.style.visibility = "visible";
+  }, 350);
+
   setTimeout(function () {
     saved.style.visibility = "hidden";
-  }, 390);
+    inputBtn.style.visibility = "visible";
+  }, 990);
+});
+// "Saved Tab" NOTIFICATION
+savetabBtn.addEventListener("click", function () {
+  savetabBtn.style.visibility = "hidden";
+  setTimeout(function () {
+    savedtab.style.visibility = "visible";
+  }, 350);
+
+  setTimeout(function () {
+    savedtab.style.visibility = "hidden";
+    savetabBtn.style.visibility = "visible";
+  }, 990);
 });
